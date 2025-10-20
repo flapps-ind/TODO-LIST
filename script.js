3f3882258579b98b3d99ba1dd066d67312b808a7
flapps-ind <mahfooz.mq@gmail.com>
2025-10-20 19:24:06 +0530

    remove button

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const textBox = document.querySelector('.textBox');
 let todoCode;
 let finalCode = '';
 textBox.addEventListener('keydown', (event)=> {
-  if(event.key == 'Enter') {
-    todoCode = `<div>${textBox.value}<button class="clearButton">remove</button></div>`;
+  if(event.key == 'Enter' && textBox.value.trim() !== '') {
+    todoCode = `<div class='todo'>${textBox.value}<button class="clearButton" onclick='removeTODO(this);'>remove</button></div>`;
     finalCode = finalCode + ' ' + todoCode;
     textBox.value = '';
   }
-  console.log(finalCode);
+  
   document.querySelector('.todoList').innerHTML = finalCode;
+  
 })
 
+const removeTODO = (buttonElement) => {
+
+    const todoItem = buttonElement.parentElement;
+
+    todoItem.remove();
+  }
